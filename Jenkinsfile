5fb951ddfd9fad09949ae1c625d79237433b5992
Sangam Birajdar <Birajdars24@gmail.com>
2023-04-05 22:37:18 +0530

    inital commit

---
+++ Jenkinsfile
@@ -0,0 +1,5 @@
+unit_test()
+static_code_analysis()
+build()
+docker_handler()
+deploy_to dev
---
+++ pipeline_config.groovy
@@ -0,0 +1,4 @@
+libraries{ 
+  merge = true  // merge means that the project configuration file will append to this location
+   sonarqube // common
+ }
