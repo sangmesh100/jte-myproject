5dbfedba21a47fcbb35cb88bb94b539f17844888
Sangam Birajdar <birajdars24@gmail.com>
2023-04-05 22:54:27 +0530

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,7 +1,7 @@
 libraries { 
     maven { 
         build_params = "mvn -DpropA=valueA clean package"  // here you can write custom parameters
-         test_param = "some test params i don't konw"
+        
      } 
     
 }
