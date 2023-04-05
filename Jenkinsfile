8181193f6d4cd4cd13e666cac9194c8a0793b567
Sangam Birajdar <Birajdars24@gmail.com>
2023-04-05 22:49:32 +0530

    first commit

--- Jenkinsfile
+++
@@ -1,5 +0,0 @@
-unit_test()
-static_code_analysis()
-build()
-docker_handler()
-deploy_to dev
--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,4 +1,9 @@
-libraries{ 
-  merge = true  // merge means that the project configuration file will append to this location
-   sonarqube // common
- }
+libraries { 
+    maven { 
+        build_params = "mvn -DpropA=valueA clean package"  // here you can write custom parameters
+         test_param = "some test params i don't konw"
+     } 
+    docker { 
+        docker_repository = "somewhere stores docker images"
+     } 
+}
